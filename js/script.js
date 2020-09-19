246f47d5b02b7f65d27e8fef72afc3da5590719e
elsonss1988 <elsonss1988@gmail.com>
2020-09-19 04:54:37 -0300

    add arrow and direction

--- js/script.js
+++ js/script.js
@@ -1,8 +1,38 @@
+console.log("cobrinha");
 let canvas=document.getElementById("snake");
 let context=canvas.getContext("2d");
 let box=32;
+let snake =[];
+snake[0]={
+    x:0*box,
+    y:0*box
+}
+let direction="right"
 function criarBG(){
     context.fillStyle="lightgreen";
-    context.fillReact(0,0,16*box,16*box));
+    context.fillRect(0,0,16*box,16*box);
 }
-criarBG();
+function criarCobrinha(){
+    for(var i=0;i<snake.length;i++){
+        context.fillStyle="green";
+        context.fillRect(snake[i].x, snake[i].y,box,box);
+    }
+}
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+    let snakex=snake[0].x;
+    let snakey=snake[0].y;
+    if(direction=="right") snakex+= box;
+    if(direction=="left") snakex-=box;
+    if(direction=="up") snakey-=box;
+    if(direction=="down") snakey+=box
+    snake.pop();
+    let newHead={
+        x:snakex,
+        y:snakey
+    }
+    snake.unshift(newhead)
+}
+
+let jogo =setInterval(iniciarJogo, 100);
